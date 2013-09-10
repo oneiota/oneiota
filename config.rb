70b58eddbcfd5e58036379937ffd018c185c5cb0
David Fontain <hello@studiooneiota.com>
2013-09-10 15:52:53 +1000

    finalised APIs. Behance,Dribbble & Pintrest added

--- config.rb
+++ config.rb
@@ -17,6 +17,17 @@ end
 #prettyURL's
 activate :directory_indexes
 
+###
+# Twitter Gem configure
+###
+  
+Twitter.configure do |config|
+  config.consumer_key = 'LwohjV8Aui2hRqiOdGH27g'
+  config.consumer_secret = 'GuGYzU47Y5ijUuulZJgYyVqQ9xbD9LAQOBQH0QxjQq4'
+  config.oauth_token = '100129471-FMQnUYD7VHNYsCd8tlpxBiXzkIpEcBYHf3k3tHKY'
+  config.oauth_token_secret = 'f52WfhoYW32lgyv7ppssMd9N0gE6an944mK9DIZQ'
+end
+
 ### 
 # Compass
 ###
@@ -86,7 +97,7 @@ configure :build do
   
   # Minify Javascript on build
   activate :minify_javascript
-  
+
   # require 'imageresizer'
   # activate :image_resizer do |i|
   #   i.input_folders = ['/feed']
--- helpers/site_helpers.rb
+++ helpers/site_helpers.rb
@@ -30,4 +30,97 @@ module SiteHelpers
     projects
   end  
 
+  def recent_pins
+    begin 
+      getpins = Faraday.new(:url => 'https://ismaelc-pinterest.p.mashape.com') do |faraday|
+        faraday.request  :url_encoded             # form-encode POST params
+        faraday.response :logger                  # log requests to STDOUT
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      end
+      recent_pins = getpins.get do |req|
+        req.url '/pins', :u => 'oneiota'
+        req.headers['X-Mashape-Authorization'] = 'jGAjGHCTdlhTdzTM8COm8BQ1ButErG0b'
+      end
+      if recent_pins
+        recent_pins = JSON.parse(recent_pins.body)
+        recent_pins
+      else
+        false
+      end
+    rescue Exception
+      puts e.message, e.backtrace
+    end 
+  end
+
+  def tweets
+    begin
+      tweets = Twitter.user_timeline("oneiota_", {:count => 20, :trim_user => 1, :exclude_replies => 1, :include_entities => 1})
+      if !tweets
+        false
+      else
+        tweets
+      end
+
+    rescue Exception
+      puts e.message, e.backtrace
+    end 
+  end
+
+  def dribbbles
+    begin
+      dribbble = Faraday.get 'http://api.dribbble.com/players/yarnell/shots.json'
+      if dribbble
+        dribbble = JSON.parse(dribbble.body)
+        dribbble["shots"]
+      else
+        false
+      end
+
+    rescue Exception
+      puts e.message, e.backtrace
+    end 
+
+  end
+
+  def behances
+    begin
+      behance = Faraday.get 'http://www.behance.net/v2/users/jessicawalsh/projects?api_key=n8MGFFD05lRU0DK8rJ5t5fkmq57XwszW'
+      if behance
+        behance = JSON.parse(behance.body)
+        behance["projects"]
+      else
+        false
+      end
+
+    rescue Exception
+      puts e.message, e.backtrace
+    end 
+  end
+
+  def clean_tweet tweet
+
+    #regexps
+    url = /( |^)http:\/\/([^\s]*\.[^\s]*)( |$)/
+    user = /@(\w+)/
+
+    #replace @usernames with links to that user
+    while tweet =~ user
+        tweet.sub! "@#{$1}", "<a href='http://twitter.com/#{$1}' >&#64#{$1}</a>"
+    end
+
+    #replace urls with links
+    while tweet =~ url
+        name = $2
+        tweet.sub! /( |^)http:\/\/#{name}( |$)/, " <a href='http://#{name}' >#{name}</a> "
+    end
+
+    tweet
+
+  end
+
+  def clean_date date_created
+    date_created = Time.at(date_created)
+    date_created
+  end
+
 end
